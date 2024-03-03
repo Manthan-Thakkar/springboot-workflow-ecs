46d25dc1aaa51dec7ecbdb541ba10f7cd36a55d2
Manthan-Thakkar <62873934+Manthan-Thakkar@users.noreply.github.com>
2024-03-03 23:35:33 +0530

    Update get_release.sh

--- get_release.sh
+++ get_release.sh
@@ -1,6 +1,6 @@
 output=$(curl -L -s \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer $1" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$GITHUB_REPOSITORY/releases | jq -r '.[].tag_name')
 
